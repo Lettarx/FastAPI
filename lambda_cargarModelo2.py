fce931d577b7ce2b674b2f0b0912fc8008f53743
HelpGamesC <117395908+HelpGamesC@users.noreply.github.com>
2024-02-26 22:28:40 -0300

    Update lambda_cargarModelo2.py
    Update 26/02

--- lambda_cargarModelo2.py
+++ lambda_cargarModelo2.py
@@ -10,9 +10,9 @@ app = FastAPI()
 
 class ImageInput(BaseModel):
     imagen_base64: str
-#BLUR
-@app.post("/evaluar_defectos/")
-def evaluar_defectos(image_input: ImageInput):
+
+@app.post("/evaluar_defectos/ImgBorrosa/")
+def evaluar_defectos_borrosa(image_input: ImageInput):
     try:
         # Decodificar la imagen base64
         imagen_bytes = base64.b64decode(image_input.imagen_base64)
@@ -20,7 +20,7 @@ def evaluar_defectos(image_input: ImageInput):
         img = cv2.imdecode(imagen_np, cv2.IMREAD_COLOR)
 
         # Cargar el modelo (reemplaza esta parte con la lógica de carga de tu modelo)
-        model_path = 'best.pt'
+        model_path = 'epoch_295_640_borrosa.pt'
         model = torch.hub.load('./', 'custom', model_path, source='local', force_reload=True)  # Puede dar error
 
         model.eval()
@@ -33,17 +33,24 @@ def evaluar_defectos(image_input: ImageInput):
         # Suponiendo que el modelo devuelve un DataFrame de pandas con las detecciones
         data = results.pandas().xyxy[0]
 
-        # Convertir a formato JSON y devolver como resultado
-        result_json = data.to_json(orient='records')
-        return {
-            'statusCode': 200,
-            'body': result_json
-        }
+        # Procesar los resultados
+        confidence = data['confidence']
+        label = data['name']
+
+        # Formatear los resultados como un diccionario
+        resultados_formateados = []
+        for i in range(len(label)):
+            resultados_formateados.append({
+                "label": label[i],
+                "confidence": round(confidence[i] * 100, 1)
+            })
+
+        return {"docs": resultados_formateados}
 
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
         
-@app.post("/evaluar_defectos_oscuros/")
+@app.post("/evaluar_defectos/ImgOscura/")
 def evaluar_defectos_oscuros(image_input: ImageInput):
     try:
         # Decodificar la imagen base64
@@ -52,7 +59,7 @@ def evaluar_defectos_oscuros(image_input: ImageInput):
         img = cv2.imdecode(imagen_np, cv2.IMREAD_COLOR)
 
         # Cargar el modelo (reemplaza esta parte con la lógica de carga de tu modelo)
-        model_path = 'best.pt'
+        model_path = 'best_160_oscura.pt'
         model = torch.hub.load('./', 'custom', model_path, source='local', force_reload=True)  # Puede dar error
 
         model.eval()
@@ -65,17 +72,24 @@ def evaluar_defectos_oscuros(image_input: ImageInput):
         # Suponiendo que el modelo devuelve un DataFrame de pandas con las detecciones
         data = results.pandas().xyxy[0]
 
-        # Convertir a formato JSON y devolver como resultado
-        result_json = data.to_json(orient='records')
-        return {
-            'statusCode': 200,
-            'body': result_json
-        }
+        # Procesar los resultados
+        confidence = data['confidence']
+        label = data['name']
+
+        # Formatear los resultados como un diccionario
+        resultados_formateados = []
+        for i in range(len(label)):
+            resultados_formateados.append({
+                "label": label[i],
+                "confidence": round(confidence[i] * 100, 1)
+            })
+
+        return {"docs": resultados_formateados}
 
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
         
-@app.post("/evaluar_defectos_persona/")
+@app.post("/evaluar_defectos/ImgPersona/")
 def evaluar_defectos_persona(image_input: ImageInput):
     try:
         # Decodificar la imagen base64
@@ -84,7 +98,7 @@ def evaluar_defectos_persona(image_input: ImageInput):
         img = cv2.imdecode(imagen_np, cv2.IMREAD_COLOR)
 
         # Cargar el modelo (reemplaza esta parte con la lógica de carga de tu modelo)
-        model_path = 'best_236_160.pt'
+        model_path = 'best_236_160_personas.pt'
         model = torch.hub.load('./', 'custom', model_path, source='local', force_reload=True)  # Puede dar error
 
         model.eval()
@@ -97,17 +111,24 @@ def evaluar_defectos_persona(image_input: ImageInput):
         # Suponiendo que el modelo devuelve un DataFrame de pandas con las detecciones
         data = results.pandas().xyxy[0]
 
-        # Convertir a formato JSON y devolver como resultado
-        result_json = data.to_json(orient='records')
-        return {
-            'statusCode': 200,
-            'body': result_json
-        }
+        # Procesar los resultados
+        confidence = data['confidence']
+        label = data['name']
+
+        # Formatear los resultados como un diccionario
+        resultados_formateados = []
+        for i in range(len(label)):
+            resultados_formateados.append({
+                "label": label[i],
+                "confidence": round(confidence[i] * 100, 1)
+            })
+
+        return {"docs": resultados_formateados}
 
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
         
-@app.post("/evaluar_defectos_iluminacion/")
+@app.post("/evaluar_defectos/ImgIluminacion/")
 def evaluar_defectos_iluminacion(image_input: ImageInput):
     try:
         # Decodificar la imagen base64
@@ -116,7 +137,7 @@ def evaluar_defectos_iluminacion(image_input: ImageInput):
         img = cv2.imdecode(imagen_np, cv2.IMREAD_COLOR)
 
         # Cargar el modelo (reemplaza esta parte con la lógica de carga de tu modelo)
-        model_path = 'best_640.pt'
+        model_path = 'best_640_iluminada.pt'
         model = torch.hub.load('./', 'custom', model_path, source='local', force_reload=True)  # Puede dar error
 
         model.eval()
@@ -129,12 +150,97 @@ def evaluar_defectos_iluminacion(image_input: ImageInput):
         # Suponiendo que el modelo devuelve un DataFrame de pandas con las detecciones
         data = results.pandas().xyxy[0]
 
-        # Convertir a formato JSON y devolver como resultado
-        result_json = data.to_json(orient='records')
-        return {
-            'statusCode': 200,
-            'body': result_json
-        }
+        # Procesar los resultados
+        confidence = data['confidence']
+        label = data['name']
+
+        # Formatear los resultados como un diccionario
+        resultados_formateados = []
+        for i in range(len(label)):
+            resultados_formateados.append({
+                "label": label[i],
+                "confidence": round(confidence[i] * 100, 1)
+            })
+
+        return {"docs": resultados_formateados}
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+        
+@app.post("/evaluar_defectos/ParedesEsquinas/")
+def evaluar_defectos_paredes(image_input: ImageInput):
+    try:
+        # Decodificar la imagen base64
+        imagen_bytes = base64.b64decode(image_input.imagen_base64)
+        imagen_np = np.frombuffer(imagen_bytes, dtype=np.uint8)
+        img = cv2.imdecode(imagen_np, cv2.IMREAD_COLOR)
+
+        # Cargar el modelo (reemplaza esta parte con la lógica de carga de tu modelo)
+        model_path = 'best_320_paredesesquinas.pt'
+        model = torch.hub.load('./', 'custom', model_path, source='local', force_reload=True)  # Puede dar error
+
+        model.eval()
+
+        # Ejecutar el modelo en la imagen
+        # Cambia esta parte según los requisitos de entrada de tu modelo
+        results = model(img)
+
+        # Procesar los resultados
+        # Suponiendo que el modelo devuelve un DataFrame de pandas con las detecciones
+        data = results.pandas().xyxy[0]
+
+        # Procesar los resultados
+        confidence = data['confidence']
+        label = data['name']
+
+        # Formatear los resultados como un diccionario
+        resultados_formateados = []
+        for i in range(len(label)):
+            resultados_formateados.append({
+                "label": label[i],
+                "confidence": round(confidence[i] * 100, 1)
+            })
+
+        return {"docs": resultados_formateados}
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+        
+@app.post("/evaluar_defectos/BLUR/")
+def evaluar_defectos_blur(image_input: ImageInput):
+    try:
+        # Decodificar la imagen base64
+        imagen_bytes = base64.b64decode(image_input.imagen_base64)
+        imagen_np = np.frombuffer(imagen_bytes, dtype=np.uint8)
+        img = cv2.imdecode(imagen_np, cv2.IMREAD_COLOR)
+
+        # Cargar el modelo (reemplaza esta parte con la lógica de carga de tu modelo)
+        model_path = 'best_640_blur.pt'
+        model = torch.hub.load('./', 'custom', model_path, source='local', force_reload=True)  # Puede dar error
+
+        model.eval()
+
+        # Ejecutar el modelo en la imagen
+        # Cambia esta parte según los requisitos de entrada de tu modelo
+        results = model(img)
+
+        # Procesar los resultados
+        # Suponiendo que el modelo devuelve un DataFrame de pandas con las detecciones
+        data = results.pandas().xyxy[0]
+
+        # Procesar los resultados
+        confidence = data['confidence']
+        label = data['name']
+
+        # Formatear los resultados como un diccionario
+        resultados_formateados = []
+        for i in range(len(label)):
+            resultados_formateados.append({
+                "label": label[i],
+                "confidence": round(confidence[i] * 100, 1)
+            })
+
+        return {"docs": resultados_formateados}
 
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
